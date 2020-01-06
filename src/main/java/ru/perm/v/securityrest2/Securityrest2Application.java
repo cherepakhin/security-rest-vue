60672ad1edadab6178d078ec53a08fbd4c4d4015
Cherepakhin Vasili <vasi.che@gmail.com>
2020-01-06 12:07:50 +0500

    Заготовка spring-boot

---
+++ src/main/java/ru/perm/v/securityrest2/Securityrest2Application.java
@@ -0,0 +1,13 @@
+package ru.perm.v.securityrest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityrest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securityrest2Application.class, args);
+	}
+
+}
